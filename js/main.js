e336b34eb254ea1c9cca9387922f5b3244649675
Lee Pender <lpender@gmail.com>
2018-10-09 12:50:38 -0400

    New Design (#3)
    * Massive changes
    
    * Bavaria constellations
    
    * New icons
    
    * Fix layout bug
    
    * Add Rhombus
    
    * Space Vector
    
    * Update copy and style
    
    * Compress images, update text
    
    * Add FB messenger, update colors, copy
    
    * Update copy
    
    * Remove low-res image

--- js/main.js
+++ js/main.js
@@ -1,63 +1,75 @@
-(function () {
-	var header = document.getElementById("mainHeader");
+(function() {
+  var header = document.getElementById('mainHeader');
 
-	function changeHeader() {
-		var scrollTop = document.documentElement.scrollTop || document.body.scrollTop;
-		header.classList.toggle("header-background", scrollTop >= 50 || document.body.classList.contains("nav-open"));
-	}
+  function changeHeader() {
+    var scrollTop =
+      document.documentElement.scrollTop || document.body.scrollTop;
+    header.classList.toggle(
+      'header-background',
+      scrollTop >= window.innerHeight ||
+        document.body.classList.contains('nav-open'),
+    );
+  }
 
-	var didScroll = false;
+  var didScroll = false;
 
-	$(window).scroll(function () {
-		didScroll = true;
-	});
+  $(window).scroll(function() {
+    didScroll = true;
+  });
 
-	setInterval(function() {
-		if (didScroll) {
-			didScroll = false;
-			changeHeader();
-		}
-	}, 100);
-
-	changeHeader();
-
-	document.getElementById("open-nav").addEventListener("click", function (event) {
-		event.preventDefault();
-		document.body.classList.toggle("nav-open");
-    if (document.body.classList.contains('nav-open')) {
+  setInterval(function() {
+    if (didScroll) {
+      didScroll = false;
       changeHeader();
-    } else {
-      setTimeout(function() {
-        changeHeader();
-      }, 200);
     }
-	});
+  }, 100);
 
-	$("a[href*=\\#]").on("click", function (event) {
-		event.preventDefault();
+  changeHeader();
 
-		$("html, body").animate({
-			scrollTop: $(this.hash).offset().top-60
-		}, 500);
+  document
+    .getElementById('open-nav')
+    .addEventListener('click', function(event) {
+      event.preventDefault();
+      document.body.classList.toggle('nav-open');
+      if (document.body.classList.contains('nav-open')) {
+        changeHeader();
+      } else {
+        setTimeout(function() {
+          changeHeader();
+        }, 200);
+      }
+    });
 
-		document.body.classList.remove("nav-open");
-	});
+  $('a[href*=\\#]').on('click', function(event) {
+    event.preventDefault();
+
+    $('html, body').animate(
+      {
+        scrollTop: $(this.hash).offset().top - 60,
+      },
+      500,
+    );
+
+    if (this.hash === '#hi') {
+      $('#name').focus();
+    }
+
+    document.body.classList.remove('nav-open');
+  });
 
   image = new Image();
-  image.src = "images/iphone.png";
+  image.src = 'images/iphone.png';
   image.onload = function() {
     $('.hero-iphone').addClass('active');
   };
 
   let index = 0;
   let $items = $('.only-one > span');
-  let length = $items.length
+  let length = $items.length;
 
-  setInterval( () => {
+  setInterval(() => {
     index++;
     $items.removeClass('active');
-    $($items[index%length]).addClass('active');
+    $($items[index % length]).addClass('active');
   }, 3000);
-
-
 })();
